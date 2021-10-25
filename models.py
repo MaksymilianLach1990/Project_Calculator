2762667889379a3e02116329a80dc76a45062ccd
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-10-25 16:33:33 +0200

    Add models with basic calculations

---
+++ models.py
@@ -0,0 +1,43 @@
+# Basic calculations
+
+# Add
+def add(a, b):
+    result = a + b
+    return result
+
+# Subtract
+def subtract(a, b):
+    result = a - b
+    return result
+
+# Multiplication
+def multiplication(a, b):
+    result = a * b
+    return result
+
+# Division
+def division(a, b):
+    result = a / b
+    return result
+
+# Raising to power
+def exponentiation(a, b):
+    result = a ** b
+    return result
+
+# The rest from division
+def modulo(a, b):
+    result = a % b
+    return result
+
+# Division without change
+def excellence(a, b):
+    result = a // b
+    return result
+
+# Element
+def element(a, b):
+    result = a ** (1/b)
+    return result
+
+
