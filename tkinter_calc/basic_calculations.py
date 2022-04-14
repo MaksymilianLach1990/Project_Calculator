f4755674201d3c3e4e955f277223653df1d3b139
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-04-14 15:16:44 +0200

    Add function 'check_action'

---
+++ tkinter_calc/basic_calculations.py
@@ -0,0 +1,56 @@
+# Basic calculations
+
+# Add
+def add(a, b):
+    result = a + b
+    return result
+
+# Subtract
+def subtract(a, b):
+    result = a - b
+    return result
+
+# Multiplication
+def multiplication(a, b):
+    result = a * b
+    return result
+
+# Division
+def division(a, b):
+    result = a / b
+    return result
+
+# Raising to power
+def exponentiation(a, b):
+    result = a ** b
+    return result
+
+# The rest from division
+def modulo(a, b):
+    result = a % b
+    return result
+
+# Division without change
+def excellence(a, b):
+    result = a // b
+    return result
+
+# Element
+def element(a, b):
+    result = a ** (1/b)
+    return result
+
+# Check action
+def check_action(num_a, num_b, action):
+    
+    options = {
+        '+': add,
+        '-': subtract,
+        '*': multiplication,
+        '/': division,
+        '**': exponentiation,
+        '%': modulo,
+        '√': element
+    }
+
+    return options[action](num_a, num_b)
